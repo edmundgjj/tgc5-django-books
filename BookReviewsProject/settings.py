e840a1c8bf465e1cf94537635ebe9c3f2441f82c
kunxin-chor <chorkunxin@yahoo.com>
2020-05-05 04:10:21 +0000

    Crispy forms

--- BookReviewsProject/settings.py
+++ BookReviewsProject/settings.py
@@ -41,8 +41,11 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispy_forms',
     'books'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
