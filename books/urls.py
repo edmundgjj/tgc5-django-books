52279ac4fd3c42b750196587bb9551ec03cd7415
kunxin-chor <chorkunxin@yahoo.com>
2020-05-05 04:16:10 +0000

    Removed trailing /

---
+++ books/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path, include
+import books.views
+
+urlpatterns = [
+    path('', books.views.index, name="show_book_route"),
+    path('create', books.views.create_book),
+    path('update/<book_id>', books.views.update_book, name = 'update_book_route'),
+    path('delete/<book_id>', books.views.delete_book, name = 'delete_book_route'),
+]
